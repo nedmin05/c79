3cdc4191e351a02f59684c9d27e9d37370b5227e
nedmin05 <79918934+nedmin05@users.noreply.github.com>
2021-08-29 20:25:22 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,22 +1,29 @@
-var student_array=[];
-function submit(){
-var name_1=document.getElementById("student_1").value;
-var name_2=document.getElementById("student_2").value;
-var name_3=document.getElementById("student_3").value;
-var name_4=document.getElementById("student_4").value;
+menu_list_array = ["chicken tandoori pizza","deluxe veggie pizza",
+"panner tikka pizza","veg extravaganza pizza"];
 
-student_array.push(name_1);
-student_array.push(name_2);
-student_array.push(name_3);
-student_array.push(name_4);
-
-console.log(student_array);
-document.getElementById("display_name").innerHTML=student_array;
-document.getElementById("submit_button").style.display="none";
-document.getElementById("sort_button").style.display="inline-block";
+function getmenu(){
+var htmldata="";
+menu_list_array.sort();
+        for(var i=0;i<menu_list_array.length;i++){
+            htmldata=htmldata+ menu_list_array[i] + '<br>'
+        }
+        document.getElementById("display_menu").innerHTML = htmldata;
+        //give the appropriate id name as display_menu  
 }
-function sorting(){
-student_array.sort();
-console.log(student_array);
-document.getElementById("display_name").innerHTML=student_array;
+
+function add_item(){
+var htmldata="";
+var item=document.getElementById("add_item").value;
+        // use the sort function as - menu_list_array.sort();
+        menu_list_array.sort();
+        for(var i=0;i<menu_list_array.length;i++){
+            htmldata=htmldata+menu_list_array[i]+'<br>';
+        }
+         document.getElementById("display_addedmenu").innerHTML = htmldata;		
+      }
+
+function add_top(){
+    var item=document.getElementById("add_item").value;
+    menu_list_array.push(item);
+	add_item();
 }
